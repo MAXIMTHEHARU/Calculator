0ccc4d1a82841c257c8c88f74352e9db325591ba
MAXIMTHEHARU <150433722+MAXIMTHEHARU@users.noreply.github.com>
2023-12-01 11:11:04 +0900

    Calculator level 1

--- MainActivity2.kt
+++ MainActivity2.kt
@@ -1,18 +1,24 @@
 package com.android.mycalculator
 
 fun main() {
-    var num1 = readln().toDouble()
-    var operate = '+'
-    var num2 = readln().toDouble()
+    var num1 = readln().toInt()
+    var operate = '/'
+    var num2 = readln().toInt()
+
+
 
     if(operate == '+') {
-        println("${num1}+${num2}")
+        var result = num1 + num2
+        println("${result}")
     } else if(operate == '-') {
-        println("${num1}-${num2}")
+        var result = num1 - num2
+        println("${result}")
     } else if(operate == '*') {
-        println("${num1}*${num2}")
+        var result = num1 * num2
+        println("${result}")
     } else if(operate == '/') {
-        println("${num1}/${num2}")
+        var result = num1 / num2
+        println("${result}")
     } else {
         println("잘못 된 연산자 입니다.")
     }
